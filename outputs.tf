e1f059583ab365e0e5b2ec20d5ce1e0ded576118
Callum <callumccr@gmail.com>
2020-08-11 15:46:00 +0100

    Gitlab README commit

--- outputs.tf
+++ outputs.tf
@@ -8,12 +8,12 @@ output "arn" {
 }
 
 output "id" {
-  value       = aws_appsync_function.default.*.arn
+  value       = aws_appsync_function.default.*.id
   description = "API Function ID (Formatted as ApiId-FunctionId)"
 }
 
 output "function_id" {
-  value       = aws_appsync_function.default.*.arn
+  value       = aws_appsync_function.default.*.function_id
   description = "A unique ID representing the Function object."
 }
 
