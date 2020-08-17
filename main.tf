6f4a921db3f5775ada08e63e066bd31ab0cff4dd
Callum <callumccr@gmail.com>
2020-08-17 15:42:17 +0100

    Gitlab README commit

--- main.tf
+++ main.tf
@@ -3,6 +3,8 @@ resource "aws_appsync_function" "default" {
   api_id                    = var.api_id
   data_source               = var.data_source
   name                      = var.function_name
+  description               = var.description
+  function_version          = var.function_version
   request_mapping_template  = var.request_mapping_template
   response_mapping_template = var.response_mapping_template
 }
