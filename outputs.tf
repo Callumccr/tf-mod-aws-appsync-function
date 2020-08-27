140f593c5debb0d95e7e44798e00302c26084e8b
Callum <callumccr@gmail.com>
2020-08-27 15:38:36 +0100

    Gitlab README commit

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,6 @@
 # -----------------------------------------------------------------------------
 
 output "config" {
-  value       = { for f in aws_appsync_function.default : f.name => { "name" = f.name, "id" = f.id } }
+  value       = { for f in aws_appsync_function.default : f.name => { "name" = f.name, "function_id" = f.function_id } }
   description = "The ID && Name of the functions"
 }
