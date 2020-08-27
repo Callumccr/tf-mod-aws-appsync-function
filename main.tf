767e9b58052a4059493caefcd12628c60ed2c435
Callum <callumccr@gmail.com>
2020-08-27 14:56:13 +0100

    Gitlab README commit

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "aws_appsync_function" "default" {
-  count                     = var.enabled ? 1 : 0
-  api_id                    = var.api_id
-  data_source               = var.data_source
-  name                      = var.function_name
-  description               = var.description
-  function_version          = var.function_version
-  request_mapping_template  = var.request_mapping_template
-  response_mapping_template = var.response_mapping_template
+  for_each          = var.enabled == true && length(var.functions_config) > 0 ? { for d in var.dynamodb_config : d.name => d } : {}
+  api_id            = var.api_id
+  data_source       = each.value.data_source
+  name              = each.value.name
+  description       = each.value.description
+  function_version  = each.value.function_version
+  request_template  = file("${var.template_dir}/${each.value.request_template}")
+  response_template = file("${var.template_dir}/${each.value.response_template}")
 }
